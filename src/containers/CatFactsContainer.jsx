fd05c13ae0431f68a3a6d6af4ce277649b24f038
Hannah Riley <riley.hannahm@gmail.com>
2020-05-07 16:51:39 -0700

    pagination functional

--- src/containers/CatFactsContainer.jsx
+++ src/containers/CatFactsContainer.jsx
@@ -6,7 +6,12 @@ import { fetchCatFacts } from '../services/FetchCatFacts';
 const CatFactsContainer = () => {
 
   const [catFacts, setCatFacts] = useState([]);
+  const [page, setPage] = useState(1);
 
+  const pageChanger = (upDown) => {
+    setPage(prevPage => prevPage + upDown);
+  };
+  
   useEffect(() => {
     fetchCatFacts()
       .then(res => setCatFacts(res));
@@ -14,8 +19,9 @@ const CatFactsContainer = () => {
 
   return (
     <section>
-    
-      <CatFactList catFacts={catFacts} />
+      <button onClick={() => pageChanger(1)} disabled={page * 10 >= catFacts.length}>&gt;</button>
+      <button onClick={() => pageChanger(-1)} disabled={page === 1}>&lt;</button>
+      <CatFactList catFacts={catFacts.slice((page - 1) * 10, page * 10)} />
     </section>
   );
 };
